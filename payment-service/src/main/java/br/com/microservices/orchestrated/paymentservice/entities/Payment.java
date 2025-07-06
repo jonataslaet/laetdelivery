0627c29558d63c8517ed81fa81943cb3bbcbde92
Jonatas Laet <jonataslaetprogramador@gmail.com>
2025-07-06 11:28:33 -0300

    0005-payment-service
    Created entity and repository.

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/entities/Payment.java
@@ -0,0 +1,58 @@
+package br.com.microservices.orchestrated.paymentservice.entities;
+
+import br.com.microservices.orchestrated.paymentservice.enums.PaymentStatusEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private int totalItems;
+
+    @Column(nullable = false)
+    private double totalAmount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentStatusEnum status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        var now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+        this.status = PaymentStatusEnum.PENDING;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/enums/PaymentStatusEnum.java
@@ -0,0 +1,8 @@
+package br.com.microservices.orchestrated.paymentservice.enums;
+
+
+public enum PaymentStatusEnum {
+    PENDING,
+    SUCCESS,
+    REFUND
+}
